a0ee55f0152c00164ba0fc19f928742c560685c4
Shabs <shabier.cassim@gmail.com>
2023-12-01 17:38:42 +0200

    Update main.js
    added get query parameter function

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
-//console.log("Hello, My World!");
-const myUrl2 = new URL(window.location.toLocaleString());
-console.log(myUrl2.toString);
+        // Function to get query parameters from the URL
+        function getQueryParam(parameterName) {
+            var urlParams = new URLSearchParams(window.location.search);
+            return urlParams.get(parameterName);
+        }
+
+        // Example: Get the value of the 'key' parameter
+        var keyValue = getQueryParam('key');
+
+        // Example: Get the value of the 'value' parameter
+        var valueValue = getQueryParam('value');
+
+        // Example: Log the values to the console
+        console.log('Value of "key" parameter:', keyValue);
+        console.log('Value of "value" parameter:', valueValue);
