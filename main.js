9a78b7dc854f76099b57ab8a5d4ffe848702aa74
Shabs <shabier.cassim@gmail.com>
2023-12-01 15:20:06 +0200

    test change

--- main.js
+++ main.js
@@ -1,9 +1,1 @@
-// Get the current URL
-console.log("Hello, World!");
-// var currentUrl = window.location.href;
-
-// Append or modify the query string parameters
-// var newUrl = currentUrl + (currentUrl.includes('?') ? '&' : '?') + 'key=hello&value=world';
-
-// Load the new URL
-// window.location.href = newUrl;
+console.log("Hello, My World!");
