8097a997d687f4fc956397995ea787c5b75d8a89
Shabs <shabier.cassim@gmail.com>
2023-12-01 17:28:03 +0200

    Update main.js
    added search param

--- main.js
+++ main.js
@@ -1,9 +1,3 @@
 console.log("Hello, My World!");
-// Get the current URL
-var currentUrl = window.location.href;
-
-// Append or modify the query string parameters
-var newUrl = currentUrl;
-
-// Load the new URL
-window.location.href = newUrl;
+const myUrl2 = new URL(window.location.toLocaleString());
+console.log(myUrl2);
