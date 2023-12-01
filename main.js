6d2b4e81a93a817650389d557b5320e15f4b4b12
Shabs <shabier.cassim@gmail.com>
2023-12-01 16:24:03 +0200

    Update main.js
    added dynamic url

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 console.log("Hello, My World!");
+// Get the current URL
+var currentUrl = window.location.href;
+
+// Append or modify the query string parameters
+var newUrl = currentUrl + (currentUrl.includes('?') ? '&' : '?') + 'key=hello&value=world';
+
+// Load the new URL
+window.location.href = newUrl;
